0b194baeaf79a6810223253f38578927d434261a
Agustin Treceno <agustin.treceno@gmail.com>
2013-07-13 12:56:21 +0100

    Add dist and lib folders

---
+++ dist/sandbox.js
@@ -0,0 +1,12 @@
+/*! sandbox - v0.0.0 - 2013-07-13
+* https://github.com/atreceno/sandbox
+* Copyright (c) 2013 Agustin Treceno; Licensed MIT */
+(function(exports) {
+
+  'use strict';
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
